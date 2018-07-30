c1ff1519afce5ad4aeb503be45dad700799f427c
Huso <husomc@gmail.com>
2018-07-31 01:30:51 +0800

    flask-app up and running

---
+++ static/js/init.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.tooltipped').tooltip({
+    delay: 50,
+    html: true
+  });
+});
+
+  (function($){
+    $(function(){
+
+      $('.sidenav').sidenav();
+
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
